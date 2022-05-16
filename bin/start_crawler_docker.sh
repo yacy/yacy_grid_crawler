a60c58d02689fc1e1852afad7940c21891ddf0b6
Michael Peter Christen <mc@yacy.net>
2022-05-16 19:16:06 +0200

    enhanced docker start script

--- bin/start_crawler_docker.sh
+++ bin/start_crawler_docker.sh
@@ -3,7 +3,8 @@ cd "`dirname $0`"
 
 bindhost="0.0.0.0"
 callhost=`hostname`.local
-containername=yacy_grid_crawler
+appname="YaCy Grid Crawler"
+containername=yacy-grid-crawler
 imagename=${containername}
 
 usage() { echo "usage: $0 [-p | --production]" 1>&2; exit 1; }
@@ -22,10 +23,10 @@ done
 containerRuns=$(docker ps | grep -i "${containername}" | wc -l ) 
 containerExists=$(docker ps -a | grep -i "${containername}" | wc -l ) 
 if [ ${containerRuns} -gt 0 ]; then
-  echo "Crawler container is already running"
+  echo "${appname} container is already running"
 elif [ ${containerExists} -gt 0 ]; then
   docker start ${containername}
-  echo "Crawler container re-started"
+  echo "${appname} container re-started"
 else
   if [[ $imagename != "yacy/"*":latest" ]] && [[ "$(docker images -q ${imagename} 2> /dev/null)" == "" ]]; then
       cd ..
@@ -33,12 +34,11 @@ else
       cd bin
   fi
   docker run -d --restart=unless-stopped -p ${bindhost}:8300:8300 \
-	 --link yacy_grid_minio --link yacy_grid_rabbitmq --link yacy_grid_elasticsearch --link yacy_grid_mcp \
-	 -e YACYGRID_GRID_S3_ADDRESS=admin:12345678@yacy_grid_minio:9000 \
-	 -e YACYGRID_GRID_BROKER_ADDRESS=guest:guest@yacy_grid_rabbitmq:5672 \
-	 -e YACYGRID_GRID_ELASTICSEARCH_ADDRESS=yacy_grid_elasticsearch:9300 \
-	 -e YACYGRID_GRID_MCP_ADDRESS=yacy_grid_mcp \
+	 --link yacy-grid-minio --link yacy-grid-rabbitmq --link yacy-grid-elasticsearch --link yacy-grid-mcp \
+	 -e YACYGRID_GRID_MCP_ADDRESS=yacy-grid-mcp \
 	 --name ${containername} ${imagename}
-  echo "Crawler started."
+  echo "${appname} started."
 fi
+docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.State}}\t{{.Mounts}}\t{{.Ports}}"
+
 echo "To get the app status, open http://${callhost}:8300/yacy/grid/mcp/info/status.json"
