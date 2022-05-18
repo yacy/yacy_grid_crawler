56e7ecfc622d5f7beb125c05451a6f6a1771319f
Michael Peter Christen <mc@yacy.net>
2022-05-18 17:30:57 +0200

    multi-arch start options

--- bin/start_crawler_docker.sh
+++ bin/start_crawler_docker.sh
@@ -5,16 +5,21 @@ bindhost="0.0.0.0"
 callhost=`hostname`
 appname="YaCy Grid Crawler"
 containername=yacy-grid-crawler
-imagename=${containername}
+imagename=${containername//-/_}
+dockerfile="Dockerfile"
+production=false
 
-usage() { echo "usage: $0 [-p | --production]" 1>&2; exit 1; }
+usage() { echo "usage: $0 [-p | --production | --arm32 | --arm64 ]" 1>&2; exit 1; }
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
-    -p | --production ) bindhost="127.0.0.1"; callhost="localhost"; imagename="yacy/${imagename//-/_}:latest"; shift 1;;
+    -p | --production ) production=true; shift 1;;
+    --arm32 ) imagename=${imagename}:arm32; dockerfile=${dockerfile}_arm32; shift 1;;
+    --arm64 ) imagename=${imagename}:arm64; dockerfile=${dockerfile}_arm64; shift 1;;
     -h | --help | -* | --* | * ) usage;;
   esac
 done
+if [ "$production" = true ] ; then bindhost="127.0.0.1"; callhost="localhost"; imagename="yacy/${imagename}"; fi
 
 containerRuns=$(docker ps | grep -i "${containername}" | wc -l ) 
 containerExists=$(docker ps -a | grep -i "${containername}" | wc -l ) 
@@ -26,7 +31,7 @@ elif [ ${containerExists} -gt 0 ]; then
 else
   if [[ $imagename != "yacy/"*":latest" ]] && [[ "$(docker images -q ${imagename} 2> /dev/null)" == "" ]]; then
       cd ..
-      docker build -t ${containername} .
+      docker build -t ${imagename} -f ${dockerfile} .
       cd bin
   fi
   docker run -d --restart=unless-stopped -p ${bindhost}:8300:8300 \
